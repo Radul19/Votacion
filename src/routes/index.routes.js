a7881fbd99ddfd736fa14e24b75184c013c8e50e
Radul19 <radulito19@gmail.com>
2020-10-02 22:06:11 -0400

    7

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,11 +4,11 @@ const router = Router()
 //Functions from...
 const {renderIndex,sendVote,sendFamily, renderPercent,renderResult,sendResults} = require('../controllers/index.controller')
 
-router.get('https://votacionesimb.herokuapp.com/send',renderIndex)
-router.get('https://votacionesimb.herokuapp.com/send/percent',renderPercent)
-router.post('https://votacionesimb.herokuapp.com/send/familyGrade/:grade',sendFamily)
-router.post('https://votacionesimb.herokuapp.com/send/send',sendVote)
-router.get('https://votacionesimb.herokuapp.com/send/result',renderResult)
+router.get('https://votacionesimb.herokuapp.com/',renderIndex)
+router.get('https://votacionesimb.herokuapp.com/percent',renderPercent)
+router.post('https://votacionesimb.herokuapp.com/familyGrade/:grade',sendFamily)
+router.post('https://votacionesimb.herokuapp.com/send',sendVote)
+router.get('https://votacionesimb.herokuapp.com/result',renderResult)
 
-router.post('https://votacionesimb.herokuapp.com/send/results',sendResults)
+router.post('https://votacionesimb.herokuapp.com/results',sendResults)
 module.exports = router
